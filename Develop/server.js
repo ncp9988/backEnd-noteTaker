d024ed8b6f6e4f4bd90548e0135144157381a61a
ncp9988@gmail.com <v.i.c@Chaus-MBP.hitronhub.home>
2021-12-26 13:16:31 -0600

    initial files

---
+++ Develop/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+const { db } = require('./db/db.json');
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
